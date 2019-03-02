cd7c4074ca5515f4406713edc31482ae7665535f
a.kolata <aleksanderkolata@gmail.com>
2019-03-02 23:27:40 +0100

    [TT-003] CORS configuration  modified

--- training-tracker-backend/src/main/java/pl/akolata/trainingtracker/configuration/WebMvcConfiguration.java
+++ training-tracker-backend/src/main/java/pl/akolata/trainingtracker/configuration/WebMvcConfiguration.java
@@ -1,19 +1,24 @@
 package pl.akolata.trainingtracker.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
-    private final long MAX_AGE_SECS = 3600;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
-                .maxAge(MAX_AGE_SECS);
+                .allowedMethods(
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.OPTIONS.name(),
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name()
+                );
     }
 }
