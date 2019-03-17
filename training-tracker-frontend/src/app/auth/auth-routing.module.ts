a25c4dd02f616a4742047c14cb296fdc77ca3959
Aleksander Kołata <olek.tbg.konta@gmail.com>
2019-03-17 11:38:16 +0100

    [TT-004] Routing configured

--- training-tracker-frontend/src/app/auth/auth-routing.module.ts
+++ training-tracker-frontend/src/app/auth/auth-routing.module.ts
@@ -1,7 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {SignInComponent} from "./component/sign-in/sign-in.component";
+import {SignUpComponent} from "./component/sign-up/sign-up.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: '/auth/sign-in'},
+  {path: 'sign-in', component: SignInComponent},
+  {path: 'sign-up', component: SignUpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
