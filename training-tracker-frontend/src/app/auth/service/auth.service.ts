374bf3e70fce7d7957b666a4a5b511bf0b857c8c
Aleksander Kołata <olek.tbg.konta@gmail.com>
2019-03-17 11:28:57 +0100

    [TT-004] AuthModule added

---
+++ training-tracker-frontend/src/app/auth/service/auth.service.ts
@@ -0,0 +1,11 @@
+import {Injectable} from '@angular/core';
+import {AuthModule} from "../auth.module";
+
+@Injectable({
+  providedIn: AuthModule
+})
+export class AuthService {
+
+  constructor() {
+  }
+}
