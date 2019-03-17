5d51961f49b0345bd2d77cbdd478f8b8438e6cea
Aleksander Kołata <olek.tbg.konta@gmail.com>
2019-03-17 23:27:15 +0100

    [TT-004] Home and PageNotFound components added

--- training-tracker-frontend/src/app/app.module.ts
+++ training-tracker-frontend/src/app/app.module.ts
@@ -9,12 +9,16 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {MaterialModule} from "./material.module";
 import {HeaderComponent} from './navigation/component/header/header.component';
 import {SideNavComponent} from './navigation/component/side-nav/side-nav.component';
+import {HomeComponent} from './home/component/home/home.component';
+import {PageNotFoundComponent} from './not-found/component/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SideNavComponent
+    SideNavComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
