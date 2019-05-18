aa0b4262c98e370325d40a6f6900c39e8ec913fe
Aleksander Kołata <olek.tbg.konta@gmail.com>
2019-05-18 23:28:27 +0100

    [TT-006] Sign In form added

---
+++ training-tracker-frontend/src/app/auth/component/sign-in/sign-in-form-config.ts
@@ -0,0 +1,32 @@
+import {BaseFormConfig} from '../../../utils/base-form-config';
+import {Validators} from '@angular/forms';
+import {ControlErrorMessagesBuilder} from '../../../utils/control-config';
+
+export class SignInFormConfig extends BaseFormConfig {
+
+  readonly CONTROL_USERNAME_OR_EMAIL = 'usernameOrEmail';
+  readonly CONTROL_PASSWORD = 'password';
+
+  constructor() {
+    super();
+    this.initControls();
+  }
+
+  initControls(): void {
+    this.controls.set(this.CONTROL_USERNAME_OR_EMAIL, {
+      initialValue: '',
+      validators: [Validators.required],
+      messages: new ControlErrorMessagesBuilder()
+        .required('You must provide username or email')
+        .build()
+    });
+    this.controls.set(this.CONTROL_PASSWORD, {
+      initialValue: '',
+      validators: [Validators.required],
+      messages: new ControlErrorMessagesBuilder()
+        .required('Password is required')
+        .build()
+    })
+  }
+
+}
--- training-tracker-frontend/src/app/auth/component/sign-in/sign-in.component.ts
+++ training-tracker-frontend/src/app/auth/component/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {SignInFormConfig} from './sign-in-form-config';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,10 +10,23 @@ import {Component, OnInit} from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() {
+  signInForm: FormGroup;
+  signInFormConfig: SignInFormConfig;
+  signInErrors$: Observable<string[]>;
+
+  constructor(private fb: FormBuilder) {
+    this.signInFormConfig = new SignInFormConfig();
   }
 
   ngOnInit() {
+    this.signInForm = this.buildSignInForm();
   }
 
+  onSignInSubmit(): void {
+
+  }
+
+  private buildSignInForm(): FormGroup {
+    return this.fb.group(this.signInFormConfig.getFormConfig());
+  }
 }
